07aa7a6ed15668ca2f5a2b65b6f007ef615b0eed
Vita Newstetter <vitanewstetter@ucla.edu>
2017-02-12 23:11:11 -0800

    trying commenting everything out

--- server.js
+++ server.js
@@ -58,50 +58,50 @@ app.post("/", cors(), function(req, res){
 
     console.log("posted");
 
-    var body = [];
-    req.on('data', function(chunk){
-        body.push(chunk);
-    }).on('end', function(){
-        body = Buffer.concat(body).toString();
-        var base64Data = body.replace(/data:image\/png;base64,/g,"");
-
-        var newBody = JSON.parse(base64Data);
-
-        //console.log(newBody);
-
-        var image_path = __dirname+"/public/images/";
-        image_name = shortid.generate();
-        db.collection('cards').insert( { _id: image_name, link: newBody.myLink} );
-        
-        findDocuments(db, function(docs){
-            console.log(docs);
-            db.close;
-        });
-
-
-        fs.writeFile(image_path + image_name + "-front"+'.png', newBody.front, 'base64', function(err){
-            if (err){
-                console.log(err);
-            }else{
-                console.log("saved image");
-                res.send(image_name);
-            }
-        });
-        fs.writeFile(image_path + image_name + "-back"+ '.png', newBody.back, 'base64', function(err){
-            if (err){
-                console.log(err);
-            }else{
-                console.log("saved image");
-            }
-        });
-        fs.writeFile(image_path + image_name + "-inside"+'.png', newBody.inside, 'base64', function(err){
-            if (err){
-                console.log(err);
-            }else{
-                console.log("saved image");
-            }
-        })
-    });
+    // var body = [];
+    // req.on('data', function(chunk){
+    //     body.push(chunk);
+    // }).on('end', function(){
+    //     body = Buffer.concat(body).toString();
+    //     var base64Data = body.replace(/data:image\/png;base64,/g,"");
+    //
+    //     var newBody = JSON.parse(base64Data);
+    //
+    //     //console.log(newBody);
+    //
+    //     var image_path = __dirname+"/public/images/";
+    //     image_name = shortid.generate();
+    //     db.collection('cards').insert( { _id: image_name, link: newBody.myLink} );
+    //
+    //     findDocuments(db, function(docs){
+    //         console.log(docs);
+    //         db.close;
+    //     });
+    //
+    //
+    //     fs.writeFile(image_path + image_name + "-front"+'.png', newBody.front, 'base64', function(err){
+    //         if (err){
+    //             console.log(err);
+    //         }else{
+    //             console.log("saved image");
+    //             res.send(image_name);
+    //         }
+    //     });
+    //     fs.writeFile(image_path + image_name + "-back"+ '.png', newBody.back, 'base64', function(err){
+    //         if (err){
+    //             console.log(err);
+    //         }else{
+    //             console.log("saved image");
+    //         }
+    //     });
+    //     fs.writeFile(image_path + image_name + "-inside"+'.png', newBody.inside, 'base64', function(err){
+    //         if (err){
+    //             console.log(err);
+    //         }else{
+    //             console.log("saved image");
+    //         }
+    //     })
+    })
     //res.redirect('/users/'+ image_name);
 
 
@@ -110,24 +110,24 @@ app.post("/redirect/", function(req, res){
     res.redirect('/users/'+ image_name);
 
 });
-app.get("/users/:image_name", function(req, res){
-    var id = req.params.image_name;
-    var image_path = "/images/";
-
-    db.collection('cards').find({_id: id}).toArray(function(err, docs) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        
-        console.log("DOCS:", docs);
-        
-        res.render('cardTemplate', {
-            frontImage: image_path + id + "-front.png" ,
-            backImage: image_path +id + "-back.png" ,
-            insideImage: image_path +id + "-inside.png",
-            myLink: docs[0].link
-        });
-        
-    });
+// app.get("/users/:image_name", function(req, res){
+//     var id = req.params.image_name;
+//     var image_path = "/images/";
+//
+//     db.collection('cards').find({_id: id}).toArray(function(err, docs) {
+//         assert.equal(err, null);
+//         console.log("Found the following records");
+//
+//         console.log("DOCS:", docs);
+//
+//         res.render('cardTemplate', {
+//             frontImage: image_path + id + "-front.png" ,
+//             backImage: image_path +id + "-back.png" ,
+//             insideImage: image_path +id + "-inside.png",
+//             myLink: docs[0].link
+//         });
+//
+//     });
     
     //console.log(myLink);
 
