5cdab0b8df205f177833a56b24c4abb0f75b7848
Vita Newstetter <vitanewstetter@ucla.edu>
2017-02-12 23:44:25 -0800

    testing

--- server.js
+++ server.js
@@ -65,46 +65,46 @@ app.post("/", cors(), function(req, res){
         body.push(chunk);
     }).on('end', function(){
         body = Buffer.concat(body).toString();
-        var base64Data = body.replace(/data:image\/png;base64,/g,""); })
-    //
-    //     var newBody = JSON.parse(base64Data);
-    //
-    //     //console.log(newBody);
-    //
-    //     var image_path = __dirname+"/public/images/";
-    //     image_name = shortid.generate();
-    //     db.collection('cards').insert( { _id: image_name, link: newBody.myLink} );
-    //
-    //     findDocuments(db, function(docs){
-    //         console.log(docs);
-    //         db.close;
-    //     });
-    //
-    //
-    //     fs.writeFile(image_path + image_name + "-front"+'.png', newBody.front, 'base64', function(err){
-    //         if (err){
-    //             console.log(err);
-    //         }else{
-    //             console.log("saved image");
-    //             res.send(image_name);
-    //         }
-    //     });
-    //     fs.writeFile(image_path + image_name + "-back"+ '.png', newBody.back, 'base64', function(err){
-    //         if (err){
-    //             console.log(err);
-    //         }else{
-    //             console.log("saved image");
-    //         }
-    //     });
-    //     fs.writeFile(image_path + image_name + "-inside"+'.png', newBody.inside, 'base64', function(err){
-    //         if (err){
-    //             console.log(err);
-    //         }else{
-    //             console.log("saved image");
-    //         }
-    //     })
-    //})
-    //res.redirect('/users/'+ image_name);
+        var base64Data = body.replace(/data:image\/png;base64,/g,"");
+
+        var newBody = JSON.parse(base64Data);
+
+        //console.log(newBody);
+
+        var image_path = __dirname+"/public/images/";
+        image_name = shortid.generate();
+        // db.collection('cards').insert( { _id: image_name, link: newBody.myLink} );
+        //
+        // findDocuments(db, function(docs){
+        //     console.log(docs);
+        //     db.close;
+        // });
+        //
+        //
+        // fs.writeFile(image_path + image_name + "-front"+'.png', newBody.front, 'base64', function(err){
+        //     if (err){
+        //         console.log(err);
+        //     }else{
+        //         console.log("saved image");
+        //         res.send(image_name);
+        //     }
+        // });
+        // fs.writeFile(image_path + image_name + "-back"+ '.png', newBody.back, 'base64', function(err){
+        //     if (err){
+        //         console.log(err);
+        //     }else{
+        //         console.log("saved image");
+        //     }
+        // });
+        // fs.writeFile(image_path + image_name + "-inside"+'.png', newBody.inside, 'base64', function(err){
+        //     if (err){
+        //         console.log(err);
+        //     }else{
+        //         console.log("saved image");
+        //     }
+        // })
+    })
+    res.redirect('/users/'+ image_name);
 
 
 });
